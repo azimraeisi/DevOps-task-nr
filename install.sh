f76674a68d2415056964a9ecae214c9eb4f46a81
azimraeisi <59738216+azimraeisi@users.noreply.github.com>
2023-09-16 23:19:10 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-sudo bash install-k8s.sh
-sudo bash install-helm.sh
+#sudo bash install-k8s.sh
+#sudo bash install-helm.sh
 
 cd helm-chart
 sudo bash install-helm-packages.sh
