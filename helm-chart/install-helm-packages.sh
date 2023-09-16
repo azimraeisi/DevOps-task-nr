6e483b0fdaea626ef16b8a108f8313dccad7da45
azimraeisi <59738216+azimraeisi@users.noreply.github.com>
2023-09-16 23:09:41 +0330

    Update install-helm-packages.sh

--- helm-chart/install-helm-packages.sh
+++ helm-chart/install-helm-packages.sh
@@ -2,3 +2,7 @@
 source ../vars.txt
 helm dependency build
 helm install test .
+
+sleep 30s
+POD_NAME=$(kubectl get pods -l app.kubernetes.io/name=wordpress -o jsonpath="{.items[0].metadata.name}")
+kubectl exec $POD_NAME -- /bin/sh -c "ln -s /opt/bitnami/wordpress /opt/bitnami/wordpress/wordpress"
