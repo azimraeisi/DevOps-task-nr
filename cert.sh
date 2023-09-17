a0bb3ad336596ab4afd537d12f3fa335cdccd7f2
azimraeisi <59738216+azimraeisi@users.noreply.github.com>
2023-09-17 11:46:47 +0330

    Create cert.sh

---
+++ cert.sh
@@ -0,0 +1,53 @@
+#Install the cert-manager Helm chart (More details:https://cert-manager.io/docs/installation/)
+helm repo add jetstack https://charts.jetstack.io
+helm repo update
+helm install \
+  cert-manager jetstack/cert-manager \
+  --version v1.13.0 \
+  --set installCRDs=true
+
+cat <<EOF > cert-manager.yaml
+apiVersion: cert-manager.io/v1
+kind: ClusterIssuer
+metadata:
+  name: letsencrypt-prod
+spec:
+  acme:
+    # The ACME server URL
+    server: https://acme-v02.api.letsencrypt.org/directory
+    # Email address used for ACME registration
+    email: raeesi.azim@gmail.com
+    # Name of a secret used to store the ACME account private key
+    privateKeySecretRef:
+      name: letsencrypt-prod-key
+    # Enable the HTTP-01 challenge provider
+    solvers:
+    - http01:
+        ingress:
+          class: nginx
+EOF
+
+kubectl apply -f cert-manager.yaml
+
+
+
+cat <<EOF > request-cert.yaml
+# my-certificate.yaml
+apiVersion: cert-manager.io/v1
+kind: Certificate
+metadata:
+  name: my-certificate
+  namespace: default
+spec:
+  secretName: my-certificate-tls
+  issuerRef:
+    name: letsencrypt-prod
+    kind: ClusterIssuer
+  commonName: word.raeisi.org
+  dnsNames:
+  - word.raeisi.org
+
+EOF
+
+kubectl apply -f request-cert.yaml
+
