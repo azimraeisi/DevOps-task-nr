50f688a9bac9fbfdd5ae9a03aabf2e75caaaec89
azimraeisi <59738216+azimraeisi@users.noreply.github.com>
2023-09-17 14:34:11 +0330

    Update install-helm-packages.sh

--- helm-chart/install-helm-packages.sh
+++ helm-chart/install-helm-packages.sh
@@ -4,8 +4,8 @@ helm dependency build
 
 helm install wordpress-app \
 --set phpmyadmin.ingress.hostname=${YourDomainURL} \
---set phpmyadmin.extraEnvVars[0].name=PHPMYADMIN_ABSOLUTE_URI \
---set phpmyadmin.extraEnvVars[0].value=http://$YourDomainURL \
+#--set phpmyadmin.extraEnvVars[0].name=PHPMYADMIN_ABSOLUTE_URI \
+--set phpmyadmin.extraEnvVars[0].value=${phpmyadminPage} \
 --set mysql.auth.username=${wordpressUsername} \
 --set mysql.auth.password=${wordpressPassword} \
 --set mysql.auth.rootPassword=${MysqlrootPassword} \
