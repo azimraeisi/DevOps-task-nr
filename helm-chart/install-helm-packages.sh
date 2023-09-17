1f6143c0555bddf162dcfd86db4850539cffd7ed
azimraeisi <59738216+azimraeisi@users.noreply.github.com>
2023-09-17 11:35:45 +0330

    Update install-helm-packages.sh

--- helm-chart/install-helm-packages.sh
+++ helm-chart/install-helm-packages.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 source ../vars.txt
 helm dependency build
-helm install test .
 
-sleep 60s
-POD_NAME=$(kubectl get pods -l app.kubernetes.io/name=wordpress -o jsonpath="{.items[0].metadata.name}")
-kubectl exec $POD_NAME -- /bin/sh -c "ln -s /opt/bitnami/wordpress /opt/bitnami/wordpress/wordpress"
+helm install wordpress-app \
+--set phpmyadmin.ingress.hostname=${YourDomainURL} \
+--set mysql.auth.username=${wordpressUsername} \
+--set mysql.auth.password=${wordpressPassword} \
+--set mysql.auth.rootPassword=${MysqlrootPassword} \
+--set wordpress.ingress.hostname=${YourDomainURL} \
+--set wordpress.externalDatabase.user=${MysqlUsername} \
+--set wordpress.externalDatabase.password=${MysqlPassword} \
+--set wordpress.wordpressUsername=${wordpressUsername} \
+--set wordpress.wordpressPassword=${wordpressPassword} .
